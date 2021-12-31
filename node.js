adb2912c3602ea6428aaa3737122a5ab3435eaaa
rajarajj6 <96234410+rajarajj6@users.noreply.github.com>
2021-12-31 18:23:50 +0530

    task 4

---
+++ node.js
@@ -0,0 +1,72 @@
+var Resume = {
+    "basics": {
+      "name": "Raja Nadakuditi",
+      "label": "Full Stack Developer",
+      "email": "rajarajj789@gmail.com",
+      "phone": "9966535099",
+      "summary": "To improve my skills and knowledge and implement it in organization for growth and development of organization and myself",
+      "location": {
+        "address": "musthakhan peta",
+        "postalCode": "521001",
+        "city": "Machilipatnam",
+        "State": "Andra Pradesh"
+      },
+    },
+    "education": {
+       "Degree" :
+       {"Qualification": "B.E(MECH)",
+        "Year Of Passing": "2021",
+        "Name of college/University": "lingayas institute of management and technology",
+        "CGPA": "6.1"},
+
+        "HSC" :
+        {"Qualification": "HSC(12th)",
+        "Year Of Passing": "2017",
+        "Name of college/University": "Narayana junior college",
+        "Percentage": "79"},
+
+        "SSC" :
+        {"Qualification": "SSC(10th)",
+        "Year Of Passing": "2015",
+        "Name of college/University": "A Little Flower The Leader ",
+        "Percentage": "8.3"},
+
+     
+      "courses": 
+        "FullStack Web Development (MERN)"
+      
+    },
+    "skills": [{
+      "name": "Web Development",
+      "level": "fresher",
+      "languages": [
+        "HTML",
+        "CSS",
+        "Bootstrap",
+        "react.js",
+        "Express.js",
+        "MangoDB",
+        "Nodejs",
+        "MySql"
+      ]
+    }],
+    "languages": [{
+      "language": ["English","Hindi","Telugu"],
+      "Native-Language": "Telugu"
+    }],
+    "hobbies": [{
+      "name":["playing cricket", "Online Gaming","coding"]
+    
+    }],
+    "projects": [{
+      "name": "DTH(down-t0-hole)button bit",
+      "description": "it is used to drill the hard materials like drilling,mining ",
+      
+        
+
+    }]
+  }
+
+  console.log(Resume)
+
+
---
+++ script.js
@@ -0,0 +1,60 @@
+var obj=[
+    {
+        Id:"rajaraj6",
+        firstName:"Raja",
+        lastName:"Nadakuditi",
+        email:"rajarajj789@gmail.com",
+        age:"21",
+        profession:"full stack developer",
+        city:"machilipatnam",
+        phoneNumber:"1234567890" 
+    }
+];
+for(var i=0; i<obj.length; i++){
+    console.log(`
+    ${obj[i].Id}
+    ${obj[i].firstName}
+    ${obj[i].lastName}
+    ${obj[i].email}
+    ${obj[i].age}
+    ${obj[i].profession}
+    ${obj[i].city}
+    ${obj[i].phoneNumber}
+    `);
+}
+obj.forEach(data);{
+    console.log(`
+    ${data.Id}
+    ${data.firstName}
+    ${data.lastName}
+    ${data.email}
+    ${data.age}
+    ${data.profession}
+    ${data.city}
+    ${data.phoneNumber}
+    `);
+}
+for(var key in obj){
+    console.log(`
+    ${obj[key].Id}
+    ${obj[key].firstName}
+    ${obj[key].lastName}
+    ${obj[key].email}
+    ${obj[key].age}
+    ${obj[key].profession}
+    ${obj[key].city}
+    ${obj[key].phoneNumber}
+    `);
+}
+for(var i in obj){
+    console.log(`
+    ${i.Id}
+    ${i.firstName}
+    ${i.lastName}
+    ${i.email}
+    ${i.age}
+    ${i.phoneNumber}
+    ${i.profession}
+    ${i.city}
+    `);
+}
